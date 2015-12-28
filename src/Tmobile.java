301e8908f920e3753222a1a1924707c5be685f1a
Prakash K <kprakash19940625@gmail.com>
2015-12-28 13:32:20 +0530

    Create Tmobile.java

---
+++ src/Tmobile.java
@@ -0,0 +1,10 @@
+class Tmobile{
+  public static void main(String args[]){
+    System.out.println("Teammate Names");
+    team={"Prakash","Vinod","Sravana","Satya","Govind","Kishore");
+    for(int i=0;i<6;i++)
+    {
+      System.out.println(team[i]);
+    }
+  }
+}
