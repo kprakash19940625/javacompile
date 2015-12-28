12fcfc94bd1e37b3bfc7eb249e270a7318074e34
Prakash K <kprakash19940625@gmail.com>
2015-12-28 14:23:26 +0530

    Update Java.java

--- src/Java.java
+++ src/Java.java
@@ -1,7 +1,9 @@
+package tmobile;
 import java.lang.*;
 class Java
 {
   public static void main(String args[]){
+    public TM = new Tmobile();
     System.out.println("Java is successfully run");
   }
 }
