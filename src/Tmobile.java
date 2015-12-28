e84701c11aa176cfad54882261df34a69fcf5615
Prakash K <kprakash19940625@gmail.com>
2015-12-28 13:58:55 +0530

    Update Tmobile.java

--- src/Tmobile.java
+++ src/Tmobile.java
@@ -1,7 +1,7 @@
 class Tmobile{
   public static void main(String args[]){
     System.out.println("Teammate Names");
-    team={"Prakash","Vinod","Sravana","Satya","Govind","Kishore");
+    String[] team={"Prakash","Vinod","Sravana","Satya","Govind","Kishore");
     for(int i=0;i<6;i++)
     {
       System.out.println(team[i]);
