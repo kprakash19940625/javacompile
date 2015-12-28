149c6907e9f628f94afa85e42888d8cb9bdceadf
Prakash K <kprakash19940625@gmail.com>
2015-12-28 15:59:09 +0530

    Update Java.java

--- src/Java.java
+++ src/Java.java
@@ -4,6 +4,6 @@ class Java
   public static void main(String args[]){
     Tmobile TM = new Tmobile();
     TM.tm();
-    System.out.println("Java is successfully run");
+    System.out.println("Welcome them aboard");
   }
 }
