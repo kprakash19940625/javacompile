6aa0d2e9562e4bf431930f45a5ffdc4dfc80b10f
Prakash K <kprakash19940625@gmail.com>
2015-12-28 20:21:49 +0530

    Update filetransfer.sh

--- src/filetransfer.sh
+++ src/filetransfer.sh
@@ -1,1 +1,2 @@
-$scp -i ~/local.pem /var/lib/jenkins/workspace/First\ Project/build/jar/javacompile.jar  ec2-user@ec2-52-90-246-221.compute-1.amazonaws.com:~/
+#!/bin/bash
+scp -i ~/local.pem /var/lib/jenkins/workspace/First\ Project/build/jar/javacompile.jar  ec2-user@ec2-52-90-246-221.compute-1.amazonaws.com:~/
