4bb9efae7946de415675b06b69c854cb494909f3
Prakash K <kprakash19940625@gmail.com>
2015-12-30 20:25:56 +0530

    Update filetransfer.sh

--- src/filetransfer.sh
+++ src/filetransfer.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#chmod 400 /var/lib/jenkins/files/local.pem
-env
+chmod 600 /home/ubuntu/ge.pem
 #echo "\n" | sudo chmod 400 ~/files/local.pem
-scp -i ~/files/local.pem /var/lib/jenkins/workspace/First\ Project/build/jar/javacompile.jar  ec2-user@52.91.108.150:~/
+scp -i /home/ubuntu/ge.pem /var/lib/jenkins/workspace/First\ Project/build/jar/javacompile.jar  ec2-user@52.91.108.150:~/
