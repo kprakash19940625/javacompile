661259da0b0d8758dd8c74351a83249989e2e576
Prakash K <kprakash19940625@gmail.com>
2015-12-30 18:27:35 +0530

    Update filetransfer.sh

--- src/filetransfer.sh
+++ src/filetransfer.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 whoami
-id
+su - user2
+whoami
 sudo chmod 600 /var/lib/jenkins/files/local.pem
 ls -l /var/lib/jenkins/files/local.pem
-sudo scp -i /var/lib/jenkins/files/local.pem /var/lib/jenkins/workspace/First\ Project/build/jar/javacompile.jar  ec2-user@ec2-52-91-108-150.compute-1.amazonaws.com:~/
+#sudo scp -i /var/lib/jenkins/files/local.pem /var/lib/jenkins/workspace/First\ Project/build/jar/javacompile.jar  ec2-user@ec2-52-91-108-150.compute-1.amazonaws.com:~/
