df939980899a24f7b48337663bd2d6cc1c9435a5
Prakash K <kprakash19940625@gmail.com>
2016-01-06 14:42:24 +0530

    Update Java.java

--- src/Java.java
+++ src/Java.java
@@ -2,11 +2,8 @@ import java.lang.*;
 class Java
 {
   public static void main(String args[]){
-    /*
     Tmobile TM = new Tmobile();
     TM.tm();
     System.out.println("Welcome them aboard");
-    */
-    System.out.println("Prakash it is working");
   }
 }
